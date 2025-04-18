fe63ccf004e8f9bdc8503f6207c12432d14a4637
Kiiyo <hiimkaze@gmail.com>
2025-04-19 03:17:35 +0800

    created game

--- src/Window.py
+++ src/Window.py
@@ -4,10 +4,11 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas = Canvas(self.__root, width=width, height=height, bg="white")
         self.__canvas.pack()
         self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -18,6 +19,10 @@ class Window:
         while self.__running:
             self.redraw()
         print("window closed...")
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
 
     def close(self):
         self.__running = False
